c384d0f64366db216eb8b857948956b026a6c188
sindweller2 <eternal_dante@yahoo.com>
2018-01-04 02:54:37 +0700

    Add files via upload

---
+++ Base64String.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Base64String
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Base64String))
+        Me.TextBoxPlain = New System.Windows.Forms.TextBox()
+        Me.ButtonToBase64 = New System.Windows.Forms.Button()
+        Me.TextBoxToBase64 = New System.Windows.Forms.TextBox()
+        Me.ButtonFromBase64 = New System.Windows.Forms.Button()
+        Me.TextBoxFromBase64 = New System.Windows.Forms.TextBox()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBoxPlain
+        '
+        Me.TextBoxPlain.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxPlain.BackColor = System.Drawing.Color.White
+        Me.TextBoxPlain.Location = New System.Drawing.Point(12, 12)
+        Me.TextBoxPlain.Multiline = True
+        Me.TextBoxPlain.Name = "TextBoxPlain"
+        Me.TextBoxPlain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBoxPlain.Size = New System.Drawing.Size(360, 100)
+        Me.TextBoxPlain.TabIndex = 0
+        '
+        'ButtonToBase64
+        '
+        Me.ButtonToBase64.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.ButtonToBase64.Location = New System.Drawing.Point(206, 330)
+        Me.ButtonToBase64.Name = "ButtonToBase64"
+        Me.ButtonToBase64.Size = New System.Drawing.Size(80, 23)
+        Me.ButtonToBase64.TabIndex = 1
+        Me.ButtonToBase64.Text = "To Base64"
+        Me.ButtonToBase64.UseVisualStyleBackColor = True
+        '
+        'TextBoxToBase64
+        '
+        Me.TextBoxToBase64.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxToBase64.BackColor = System.Drawing.Color.White
+        Me.TextBoxToBase64.Location = New System.Drawing.Point(12, 118)
+        Me.TextBoxToBase64.Multiline = True
+        Me.TextBoxToBase64.Name = "TextBoxToBase64"
+        Me.TextBoxToBase64.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBoxToBase64.Size = New System.Drawing.Size(360, 100)
+        Me.TextBoxToBase64.TabIndex = 2
+        '
+        'ButtonFromBase64
+        '
+        Me.ButtonFromBase64.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.ButtonFromBase64.Location = New System.Drawing.Point(292, 330)
+        Me.ButtonFromBase64.Name = "ButtonFromBase64"
+        Me.ButtonFromBase64.Size = New System.Drawing.Size(80, 23)
+        Me.ButtonFromBase64.TabIndex = 3
+        Me.ButtonFromBase64.Text = "From Base64"
+        Me.ButtonFromBase64.UseVisualStyleBackColor = True
+        '
+        'TextBoxFromBase64
+        '
+        Me.TextBoxFromBase64.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxFromBase64.BackColor = System.Drawing.Color.White
+        Me.TextBoxFromBase64.Location = New System.Drawing.Point(12, 224)
+        Me.TextBoxFromBase64.Multiline = True
+        Me.TextBoxFromBase64.Name = "TextBoxFromBase64"
+        Me.TextBoxFromBase64.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBoxFromBase64.Size = New System.Drawing.Size(360, 100)
+        Me.TextBoxFromBase64.TabIndex = 4
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ButtonClear.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.ButtonClear.Location = New System.Drawing.Point(125, 330)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonClear.TabIndex = 5
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'Base64String
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(384, 365)
+        Me.Controls.Add(Me.ButtonClear)
+        Me.Controls.Add(Me.TextBoxFromBase64)
+        Me.Controls.Add(Me.ButtonFromBase64)
+        Me.Controls.Add(Me.TextBoxToBase64)
+        Me.Controls.Add(Me.ButtonToBase64)
+        Me.Controls.Add(Me.TextBoxPlain)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Base64String"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Base64String"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBoxPlain As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonToBase64 As System.Windows.Forms.Button
+    Friend WithEvents TextBoxToBase64 As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonFromBase64 As System.Windows.Forms.Button
+    Friend WithEvents TextBoxFromBase64 As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonClear As System.Windows.Forms.Button
+
+End Class
---
+++ Base64String.vb
@@ -0,0 +1,86 @@
+﻿Public Class Base64String
+
+    Private Sub ButtonToBase64_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonToBase64.Click
+        Try
+            Me.TextBoxToBase64.Text = Convert.ToBase64String(System.Text.UnicodeEncoding.ASCII.GetBytes(Me.TextBoxPlain.Text.Trim))
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonFromBase64_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonFromBase64.Click
+        Try
+            Me.TextBoxFromBase64.Text = System.Text.UnicodeEncoding.ASCII.GetString(Convert.FromBase64String(Me.TextBoxToBase64.Text.Trim))
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBoxPlain_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBoxPlain.KeyPress
+        Try
+            If e.KeyChar = Microsoft.VisualBasic.ChrW(Keys.Return) Then
+                Me.ButtonToBase64_Click(Me, e)
+                Me.TextBoxToBase64.Focus()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBoxToBase64_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBoxToBase64.KeyPress
+        Try
+            If e.KeyChar = Microsoft.VisualBasic.ChrW(Keys.Return) Then
+                Me.ButtonFromBase64_Click(Me, e)
+                Me.TextBoxFromBase64.Focus()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBoxFromBase64_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBoxFromBase64.KeyPress
+        Try
+            If e.KeyChar = Microsoft.VisualBasic.ChrW(Keys.Return) Then
+                Me.TextBoxPlain.Text = Nothing
+                Me.TextBoxPlain.Text = Me.TextBoxFromBase64.Text
+                Me.TextBoxPlain.Focus()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBoxPlain_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBoxPlain.GotFocus
+        Try
+            Me.TextBoxPlain.SelectionStart = Me.TextBoxPlain.TextLength
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBoxToBase64_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBoxToBase64.GotFocus
+        Try
+            Me.TextBoxToBase64.SelectionStart = Me.TextBoxToBase64.TextLength
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBoxFromBase64_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBoxFromBase64.GotFocus
+        Try
+            Me.TextBoxFromBase64.SelectionStart = Me.TextBoxFromBase64.TextLength
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonClear.Click
+        Try
+            Me.TextBoxPlain.Clear()
+            Me.TextBoxToBase64.Clear()
+            Me.TextBoxFromBase64.Clear()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
